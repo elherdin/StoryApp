feb763a7a58670baa12d1fcb4b72bfc5cc970b22
Herdin Anhar <139094609+elherdin@users.noreply.github.com>
2024-05-15 17:20:10 +0700

    Add files via upload

--- app/src/main/java/com/example/intermediatesub/data/response/StoryResponse.kt
+++ app/src/main/java/com/example/intermediatesub/data/response/StoryResponse.kt
@@ -8,12 +8,6 @@ data class StoryResponse(
 
     @field:SerializedName("listStory")
     val listStory: List<ListStoryItem> = emptyList(),
-
-    @field:SerializedName("error")
-    val error: Boolean? = null,
-
-    @field:SerializedName("message")
-    val message: String? = null
 )
 
 @Parcelize
@@ -31,12 +25,6 @@ data class ListStoryItem(
     @field:SerializedName("description")
     val description: String? = null,
 
-    @field:SerializedName("lon")
-    val lon: Double? = null,
-
     @field:SerializedName("id")
     val id: String? = null,
-
-    @field:SerializedName("lat")
-    val lat: Double? = null
 ) : Parcelable
